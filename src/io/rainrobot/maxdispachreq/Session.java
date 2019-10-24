72b2428b0b3858414f07d86244e5a69b4d7eecf4
roy <roy.barak.m@gmail.com>
2019-10-24 22:21:40 +0200

    minor changes

--- src/io/rainrobot/maxdispachreq/Session.java
+++ src/io/rainrobot/maxdispachreq/Session.java
@@ -1,13 +1,15 @@
 package io.rainrobot.maxdispachreq;
 
 public interface Session {
+
+    void setToken(String token);
+
     String getToken();
 
     boolean isExpired();
 
-    ServerAddress
-
-    getSessionAddress();
+    ServerAddress getSessionAddress();
 
     void setAddress(ServerAddress address);
+
 }
