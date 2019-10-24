3d0f8160a1e02218f72c4a71e240b6f4b776a6b1
roy <roy.barak.m@gmail.com>
2019-10-25 01:19:44 +0200

    minor changes

--- src/io/rainrobot/maxdispachreq/Dispatcher.java
+++ src/io/rainrobot/maxdispachreq/Dispatcher.java
@@ -15,6 +15,4 @@ public class Dispatcher {
         ServerAddress address = addressService.getAddress(id);
         messageSender.send(msg, address);
     }
-
-
 }
--- src/io/rainrobot/maxdispachreq/RequestMsg.java
+++ src/io/rainrobot/maxdispachreq/RequestMsg.java
@@ -2,4 +2,5 @@ package io.rainrobot.maxdispachreq;
 
 public interface RequestMsg {
     String getId();
+    String getMsg();
 }
